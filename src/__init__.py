382fc50c0f62ed97ca73df4348d2e6bba145590e
Fbeunder <frank@lynxx.eu>
2025-03-03 17:41:31 +0100

    Verbeterde documentatie voor het src package

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,21 @@
-# Titanic Survival Prediction Project
-# Main package initialization
+"""
+Titanic Survival Prediction Project
+
+This package contains the Titanic Survival Prediction application, which uses machine learning
+to predict passengers' survival based on the Kaggle Titanic dataset.
+
+The project is organized into several modules:
+- data_processing: For loading and preprocessing data
+- feature_engineering: For creating and selecting features
+- modelling: For creating, training and evaluating machine learning models
+- utilities: For configuration and helper functions
+- web_interface: For the web application interface
+
+Minimum Requirements:
+- Python 3.8 or higher
+- Dependencies listed in requirements.txt
+
+For usage examples, see the README.md file or the notebooks directory.
+"""
+
+__version__ = "1.0.0"
